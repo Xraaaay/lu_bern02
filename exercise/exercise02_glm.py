b298e8d7e5db6474678ed48ad20a42c497759922
Xraaaay <xiaorw1227@gmail.com>
2026-09-04 10:01:53 +0200

    ex02: model

--- exercise/exercise02_glm.py
+++ exercise/exercise02_glm.py
@@ -9,16 +9,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# %% Load Data
+from scipy.optimize import minimize
+
+# %% Read Data
 data_path = "./data/bird_count.csv"
 df = pd.read_csv(data_path)
 df = df.sort_values(by="yr")
+
+# %% Extract Data
 years = df["yr"].to_numpy(dtype=int)
+years_centered = years - np.min(years)
 counts = df["count"].to_numpy(dtype=int)
 
-# %% Model: Poisson Regression
-# TODO
-def poisson_regression(x, y):
-    pass
+# %% Model: Poisson GLM
+def poisson_glm(x, y):
+    def log_likelihood(coef):
+        beta0, beta1 = coef
+        eta = beta0 + beta1 * x
+        return -np.sum(-np.exp(eta) + y * eta)
+    result = minimize(log_likelihood, x0=[0, 0])
+    # print(result)
+    return result.x
 
-# %%
+# %% Calculate Coefficients
+beta0, beta1 = poisson_glm(x=years_centered, y=counts)
+print(beta0, beta1)
+
+# %% Simulation
+
+# %% Plot
+fig, ax = plt.subplots()
+ax.scatter(years, counts)
+
+# %% Write Data
